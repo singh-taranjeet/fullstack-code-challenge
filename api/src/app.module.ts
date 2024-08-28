32a6aa294cbc39c16be37047a7c092d92a5652bc
Taranjeet Singh <93113908+singh-taranjeet@users.noreply.github.com>
2024-08-28 22:48:07 +1000

    Created Result module

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ApolloDriverConfig, ApolloDriver } from '@nestjs/apollo';
+import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
+import { ResultModule } from './result/result.module';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      subscriptions: {
+        'graphql-ws': true,
+      },
+    }),
+    ResultModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
